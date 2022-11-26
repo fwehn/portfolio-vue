d4c2e2999c2bf3163d7dbe8517d88acfa04c6e0c
fwehn <finn.wehn@web.de>
2022-11-26 21:25:10 +0100

    added ratings to skillset;

--- src/components/Skillset.vue
+++ src/components/Skillset.vue
@@ -2,10 +2,10 @@
   <section id="skillset" class="mainSections">
     <h2 class="text-5xl py-2">Skillset</h2>
     <div class="flex justify-center items-center">
-      <SkillsetIcon :icon="'nodejs'" :info="'NodeJS'" :linkTo="'https://nodejs.org/en/'"/>
-      <SkillsetIcon :icon="'vue'" :info="'Vue'" :linkTo="'https://vuejs.org/'"/>
-      <SkillsetIcon :icon="'mysql'" :info="'MySQL'" :linkTo="'https://www.mysql.com/de/'"/>
-      <SkillsetIcon :icon="'docker'" :info="'Docker'" :linkTo="'https://www.docker.com/'"/>
+      <SkillsetIcon :icon="'nodejs'" :info="'NodeJS'" :rating="4" :linkTo="'https://nodejs.org/en/'"/>
+      <SkillsetIcon :icon="'docker'" :info="'Docker'" :rating="4" :linkTo="'https://www.docker.com/'"/>
+      <SkillsetIcon :icon="'mysql'" :info="'MySQL'" :rating="3" :linkTo="'https://www.mysql.com/de/'"/>
+      <SkillsetIcon :icon="'vue'" :info="'Vue'" :rating="2" :linkTo="'https://vuejs.org/'"/>
     </div>
   </section>
 </template>
